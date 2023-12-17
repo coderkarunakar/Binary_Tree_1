905c469e240a88cea6e6658ff197fe4e23c934e9
Karunakar chembeti <114292518+coderkarunakar@users.noreply.github.com>
2023-12-17 15:43:34 +0530

    Terminology of tree
    Please look class notes as well for pictorial understanding

---
+++ Terminology_of_trees.py
@@ -0,0 +1,9 @@
+#Terminology:
+#Leafs:these are end nodes with out childrens 
+#Parent:which has children
+#siblings:two  childrens are called as siblings
+#Node:every child is called as Node
+#root:which is similar to head which has its children information
+#GrandParent:which is parents,parent is called as grandparent(which is an ancestor
+# child:which is a next step i.e children; of parent
+
