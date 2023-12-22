8b97db32481ac6e605a290aa07d1974a6da8a505
Karunakar chembeti <114292518+coderkarunakar@users.noreply.github.com>
2023-12-22 08:51:55 +0530

    Node with larger data

---
+++ Node_with_Large_data.py
@@ -0,0 +1,85 @@
+#Logic:Base case if Tree is None then return -1
+    # ->find the largest of left ,and largest of right ,and compare both left,right large value 
+    #and then compare that large value with the root value and return the large value 
+    #input of a binary tree
+class BinaryTreeNode:
+    #Note:when ever you create a node it should have data in its constructor
+    def __init__(self,data):
+        self.data = data
+        #initially making left ,right as None
+        self.left = None
+        self.right = None
+def printTree(root):
+    if root == None:
+        return
+    print(root.data)
+    printTree(root.left)
+    printTree(root.right)
+def printTreeDetail(root):
+    #works untill leaf node i.e which has no child ,this is the base case of recursive call
+    if root == None:
+        return
+    print(root.data, end=':')
+    if root.left != None:
+        print("L", root.left.data, end=',')
+    if root.right != None:
+        print("R", root.right.data, end=' ')
+        #this is to get into a new line
+    print()
+    printTreeDetail(root.left)
+    printTreeDetail(root.right)
+
+#its going to ask user to give input,and it returns the root of the tree
+#smaller tree input we will get through recursively
+def treeinput():
+    rootData = int(input("enter"))
+    if rootData == -1:
+        return None
+    root = BinaryTreeNode(rootData)
+    #calling above method in order to get repeat above steps
+    leftTree = treeinput()
+    rightTree = treeinput()
+    #just assigning it
+    root.left = leftTree
+    root.right = rightTree
+    return root
+
+def numnodes(root):
+    #if the root itself is none(empty ) then we dont have any root so no left ,no right 
+    if root == None:
+        return 0
+        #if we have root then definetely we do have left ,or right node,theis calls above function and repeat same,if the value doesnot exist simpy it returns 0
+    leftcount = numnodes(root.left)
+    rightcount = numnodes(root.right)
+    #becase of this +1 only we are able to add all those if we remove +1 we get ans as zero becasue from last it starts counting and left,right will be zero some point and it will add all those zeroes only ,finally we get zero,in order to get a value we need to give +1 this is interview question very important and careful
+    return 1+leftcount+rightcount
+
+
+#to get sum of each node
+def sum_of_nodes_tree(root):
+    if root is None:
+        return 0
+
+#it is actually fetching its value
+    return root.data + sum_of_nodes_tree(root.left) + sum_of_nodes_tree(root.right)
+
+#Node with large data
+def largestData(root):
+    if root == None:
+        return -1 #ideally need to return -infy but here we are returning -1
+    leftlargest = largestData(root.left)
+    rightlargest = largestData(root.right)
+    #simply finding the max out of all left,right ,and root value
+    largest =max(leftlargest,rightlargest,root.data)
+    return largest
+
+    
+root = treeinput()
+printTreeDetail(root)
+print("the no of nodes is ",numnodes(root))
+result = sum_of_nodes_tree(root)
+print("the sum of the nodes is ",result)
+print("the largest data is ",largestData(root))
+
+
+#note:in order to print none simply give -1 and first try to finish a tree ,and then next go to other tree
