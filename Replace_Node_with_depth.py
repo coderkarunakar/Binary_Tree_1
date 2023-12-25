d31d90d2cae18bb4ef17d8bdb0e1610031aa62f9
Karunakar chembeti <114292518+coderkarunakar@users.noreply.github.com>
2023-12-25 18:54:19 +0530

    updated ,_Replace node with depth

--- Replace_Node_with_depth.py
+++ Replace_Node_with_depth.py
@@ -1,5 +1,5 @@
-#Doubt and take a look pls dry run it 
-
+#this is bit difficult question dry run is there in notes pls pls do look
+#pls look class notes to understand input and output its all about Left,Root ,Right starts from down to up it was build ,   
 #Binary tree creation,Tree node class,update node value with class,inorder traversal(updating node value to a list),main function (replaceNodeWithDepth),finally execution
 
 #Replace Node with Depth
@@ -33,27 +33,38 @@ def inorderTraversal(root,result):
 def replacenodewithDepth(nodes):
     if not nodes:
         return
+        # Creates the root node of the binary tree using the first element of the nodes list.
     root = TreeNode(int(nodes[0]))
+    # Initializes a queue with the root node.
     queue = [root]
     #traversing the queue,and poping the values from it
     i = 1
+    #Tree Construction using Queue,this runs untill the queue is empty
     while queue:
-        #poping the last value from the queue
+#Dequeues the first node from the queue for the processing
         current = queue.pop(0)
-        #if the node value is not equal to -1 then pick the left value and append it to queue,same for right as well
+       #checks if the next element in the node list is not equal to -1
+       #left iteration
         if nodes[i]!= -1:
+            #creates a left child
             current.left = TreeNode(int(nodes[i]))
+            #enqueues the newly created left child node into queue further processing
             queue.append(current.left)
+            #to move to the next element in the queue
         i+=1
+        #right iteration
         if nodes[i]!= -1:
             current.right = TreeNode(int(nodes[i]))
             queue.append(current.right)
         i +=1
     #update the tree with depth 
+    #updating the node value of the tree with their respective depths starting from 0
     updateTreeDepth(root, 0)
-    #perform in order traversal to print modified tree
+    #initialize empty list to store the inorder traversal result
     result = []
+    #performs inorder traversal,and appends the node value(depths) to the results list
     inorderTraversal(root,result)
+    #prints the space seperated node value(depths) after converting them to strings
     print(' '.join(map(str, result)))
 
     
@@ -69,6 +80,9 @@ replacenodewithDepth(nodes)
 
 
         
+
+
+
 
         
      
